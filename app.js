b9137d6c2d01ac29b87a6b05c115223f87bd5368
mariane delvo <marianengd@gmail.com>
2017-03-10 16:20:24 -0600

    ignore last commit.

--- app.js
+++ app.js
@@ -40,8 +40,8 @@ app.get('/select', function(req, res) {
       return;
     } 
    	console.log("**********CONNECTING TO DATABASE**********");
-   	
-      var query = "SELECT MAX_DEPTH_PCT, \"ABSOLUTE_ODOMETER_m\" FROM '+ilisection+'";	//SELECT MAX_DEPTH_PCT, \"ABSOLUTE_ODOMETER_m\" FROM CAPSTONE_ILI_DATA_SAMPLE FETCH FIRST 5 ROWS ONLY
+   										// '+ilisection+'
+      var query = "SELECT MAX_DEPTH_PCT, \"ABSOLUTE_ODOMETER_m\" FROM CAPSTONE_ILI_DATA_SAMPLE";	//SELECT MAX_DEPTH_PCT, \"ABSOLUTE_ODOMETER_m\" FROM CAPSTONE_ILI_DATA_SAMPLE FETCH FIRST 5 ROWS ONLY
       conn.query(query, function(err, rows) {
         if (err) {
           console.log("Error: ", err);
